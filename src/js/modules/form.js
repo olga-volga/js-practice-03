65ccf874ec32fbea2c00a68e61c33df58d2d47a8
olga <yaolmi@yandex.ru>
2021-12-01 12:20:33 +0700

    optimize form.js

--- src/js/modules/form.js
+++ src/js/modules/form.js
@@ -3,13 +3,12 @@ export default class Form {
 		this.forms = document.querySelectorAll(forms);
 		this.emails = document.querySelectorAll(emails);
 		this.phone = document.querySelector(phone);
-	}
-	async postData(url, data) {
-		let result = await fetch(url, {
-			method: "POST",
-			body: data
-		});
-		return await result.text();
+		this.message = {
+			load: 'Sending...',
+			success: 'Thanks! We will contact you as soon as possible.',
+			fail: 'Error! Please, try again later.'
+		};
+		this.path = 'assets/question.php';
 	}
 	checkEmailInput() {
 		this.emails.forEach(item => {
@@ -45,11 +44,9 @@ export default class Form {
 			if (def.length >= val.length) {
 				val = def;
 			}
-
 			this.value = matrix.replace(/./g, function(a) {
 				return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
 			});
-
 			if (event.type === 'blur') {
 				if (this.value.length == 2) {
 					this.value = '';
@@ -58,10 +55,16 @@ export default class Form {
 				setCursorPosition(this.value.length, this);
 			}
 		}
-
 		this.phone.addEventListener('input', createMask);
 		this.phone.addEventListener('focus', createMask);
 		this.phone.addEventListener('blur', createMask);
+	}
+	async postData(url, data) {
+		let result = await fetch(url, {
+			method: "POST",
+			body: data
+		});
+		return await result.text();
 	}
 	render() {
 		this.checkEmailInput();
@@ -71,31 +74,33 @@ export default class Form {
 			item.addEventListener('submit', (e) => {
 				e.preventDefault();
 
-				let formData = new FormData(item);
-
 				let statusMessage = document.createElement('div');
 				statusMessage.classList.add('status', 'animated', 'fadeIn');
 				statusMessage.style.cssText = 'color: #000; font-size: 26px;margin-top: 32px;';
+				statusMessage.textContent = this.message.load;
+
 				item.parentNode.append(statusMessage);
 
 				item.classList.add('animated', 'fadeOut');
 				item.style.display = 'none';
 
-				this.postData('assets/question.php', formData)
+				let formData = new FormData(item);
+
+				this.postData(this.path, formData)
 					.then(res => {
 						console.log(res);
-						statusMessage.textContent = 'Thanks! We will contact you as soon as possible.';
+						statusMessage.textContent = this.message.success;
 					})
 					.catch(() => {
-						statusMessage.textContent = 'Error! Please, try again later.';
+						statusMessage.textContent = this.message.fail;
 					})
 					.finally(() => {
+						item.reset();
 						setTimeout(() => {
 							statusMessage.remove();
 							item.classList.remove('animated', 'fadeOut');
 							item.classList.add('animated', 'fadeIn');
 							item.style.display = '';
-							item.reset();
 						}, 5000);
 						
 					})
